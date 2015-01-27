65ac23de21cd485cd5712fb43f519a97a80fca55
Chris <ctarwater@madsecurity.com>
2015-01-27 15:50:19 -0800

    Initial

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install dependencies (debbootstrap)
+sudo apt-get install debootstrap
+
+# Fetch the latest Kali debootstrap script from git
+curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
+
+# Run Kali debootstrap to create the install
+sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
+
+# Bundle the install
+sudo tar -C kali-root -c . | sudo docker import - blackfinsecurity/kali &&\
+
+# Remove the artifacts
+sudo rm -rf ./kali-root &&\
+
+# Run the image to make sure it worked
+docker run -t -i blackfinsecurity/kali cat /etc/debian_version &&\
+
+# Let us know if it worked or not
+echo "Build OK" || echo "Build failed!" 
